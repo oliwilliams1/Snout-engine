f884405e9298ae2af43912ec2c9e0ec44daee82c
oliwilliams1 <imhungryoliver@gmail.com>
2024-05-30 18:53:09 +1200

    Simple dot, added OGL_DEV include file

--- main.cpp
+++ main.cpp
@@ -1,8 +1,37 @@
 #include <stdio.h>
 #include <glew.h>
 #include <glut.h>
+#include <ogldev_math_3d.h>
 
-int main(int argc, char* argv[])
+GLuint VBO;
+
+static void RenderSceneCB()
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+	glDisableVertexAttribArray(0);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+	glDrawArrays(GL_POINTS, 0, 1);
+
+	glDisableVertexAttribArray(0);
+
+	glutSwapBuffers();
+}
+
+static void createVertexBuffer()
+{
+	Vector3f Vertices[1];
+	Vertices[0] = Vector3f(0.0f, 0.0f, 0.0f);
+
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
+}
+int main(int argc, char** argv)
 {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_ALPHA);
@@ -14,12 +43,21 @@ int main(int argc, char* argv[])
 	int y = 100;
 	glutInitWindowPosition(x, y);
 	int win = glutCreateWindow("Render Engine");
-	printf("window id: %d\n", win);
+	
+	// must be done after glut init
+	GLenum res = glewInit();
+	if (res != GLEW_OK) {
+		fprintf(stderr, "Erroor: '%s'\n", glewGetErrorString(res));
+	}
 
 	GLclampf Red = 0.0f, Green = 0.0f, Blue = 0.0f, Alpha = 0.0f;
 	glClearColor(Red, Green, Blue, Alpha);
 
-	//glutDisplayFunc(RencderSceneCB);
+	createVertexBuffer();
+
+	glutDisplayFunc(RenderSceneCB);
+
 	glutMainLoop();
+
 	return 0;
 }
