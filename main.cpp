8e93882fdf924d758a82bc9058a021f91890015e
oliwilliams1 <imhungryoliver@gmail.com>
2024-06-01 17:57:41 +1200

    Shaders

--- main.cpp
+++ main.cpp
@@ -3,7 +3,90 @@
 #include <glut.h>
 #include <ogldev_math_3d.h>
 
+using std::string;
+
 GLuint VBO;
+
+const char* pVSFileName = "shader.vert";
+const char* pFSFileName = "shader.frag";
+
+static void AddShader(GLuint ShaderProgram, const char* pShaderText, GLenum ShaderType)
+{
+    GLuint ShaderObj = glCreateShader(ShaderType);
+
+    if (ShaderObj == 0) {
+        fprintf(stderr, "Error creating shader type %d\n", ShaderType);
+        exit(0);
+    }
+
+    const GLchar* p[1];
+    p[0] = pShaderText;
+
+    GLint Lengths[1];
+    Lengths[0] = (GLint)strlen(pShaderText);
+
+    glShaderSource(ShaderObj, 1, p, Lengths);
+
+    glCompileShader(ShaderObj);
+
+    GLint success;
+    glGetShaderiv(ShaderObj, GL_COMPILE_STATUS, &success);
+
+    if (!success) {
+        GLchar InfoLog[1024];
+        glGetShaderInfoLog(ShaderObj, 1024, NULL, InfoLog);
+        fprintf(stderr, "Error compiling shader type %d: '%s'\n", ShaderType, InfoLog);
+        exit(1);
+    }
+
+    glAttachShader(ShaderProgram, ShaderObj);
+}
+
+static void CompileShaders()
+{
+    GLuint ShaderProgram = glCreateProgram();
+
+    if (ShaderProgram == 0) {
+        fprintf(stderr, "Error creating shader program\n");
+        exit(1);
+    }
+
+    std::string vs, fs;
+
+    if (!ReadFile(pVSFileName, vs)) {
+        exit(1);
+    };
+
+    AddShader(ShaderProgram, vs.c_str(), GL_VERTEX_SHADER);
+
+    if (!ReadFile(pFSFileName, fs)) {
+        exit(1);
+    };
+
+    AddShader(ShaderProgram, fs.c_str(), GL_FRAGMENT_SHADER);
+
+    GLint Success = 0;
+    GLchar ErrorLog[1024] = { 0 };
+
+    glLinkProgram(ShaderProgram);
+
+    glGetProgramiv(ShaderProgram, GL_LINK_STATUS, &Success);
+    if (Success == 0) {
+        glGetProgramInfoLog(ShaderProgram, sizeof(ErrorLog), NULL, ErrorLog);
+        fprintf(stderr, "Error linking shader program: '%s'\n", ErrorLog);
+        exit(1);
+    }
+
+    glValidateProgram(ShaderProgram);
+    glGetProgramiv(ShaderProgram, GL_VALIDATE_STATUS, &Success);
+    if (!Success) {
+        glGetProgramInfoLog(ShaderProgram, sizeof(ErrorLog), NULL, ErrorLog);
+        fprintf(stderr, "Invalid shader program: '%s'\n", ErrorLog);
+        exit(1);
+    }
+
+    glUseProgram(ShaderProgram);
+}
 
 static void RenderSceneCB()
 {
@@ -11,11 +94,11 @@ static void RenderSceneCB()
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 
-	glDisableVertexAttribArray(0);
+	glEnableVertexAttribArray(0);
 
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
 
-	glDrawArrays(GL_POINTS, 0, 1);
+	glDrawArrays(GL_TRIANGLES, 0, 3);
 
 	glDisableVertexAttribArray(0);
 
@@ -24,8 +107,12 @@ static void RenderSceneCB()
 
 static void createVertexBuffer()
 {
-	Vector3f Vertices[1];
-	Vertices[0] = Vector3f(0.0f, 0.0f, 0.0f);
+	glEnable(GL_CULL_FACE);
+	glFrontFace(GL_CW);
+	Vector3f Vertices[3];
+	Vertices[0] = Vector3f(-1.0f, -1.0f, 0.0f);
+	Vertices[1] = Vector3f(0.0f, 1.0f, 0.0f);
+	Vertices[2] = Vector3f(1.0f, -1.0f, 0.0f);
 
 	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
@@ -42,7 +129,6 @@ int main(int argc, char** argv)
 	int x = 200;
 	int y = 100;
 	glutInitWindowPosition(x, y);
-	glutInitWindowSize(width, height);
 	int win = glutCreateWindow("Hello World");
 	
 	// must be done after glut init
@@ -55,6 +141,8 @@ int main(int argc, char** argv)
 	glClearColor(Red, Green, Blue, Alpha);
 
 	createVertexBuffer();
+
+	CompileShaders();
 
 	glutDisplayFunc(RenderSceneCB);
 
